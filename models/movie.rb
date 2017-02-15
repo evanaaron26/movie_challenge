e69954c24be45bdf0b15ed17f02d3ef58408a1bf
evanaaron26 <evanaaron26@yahoo.com>
2017-02-14 21:30:46 -0600

    initialize model

--- models/movie.rb
+++ models/movie.rb
@@ -1,3 +1,21 @@
 class Movie
+
+    def initialize
+        @runtime = movie_list.json["time"]
+        @title = movie_list.json["title"]
+        @rating = movie_list.json["rating"]
+    end 
+
+    def run_time
+        runtime_split = movie.runtime.split(" ")
+        (runtime_split[0].to_i * 60) + runtime_split[2].to_i
+    end 
   
 end
+
+
+# runtime_split = movie.runtime.split(" ")
+# [1, "hr", 44, min]
+
+# (runtime_split[0].to_i * 60) + runtime_split[2].to_i
+
